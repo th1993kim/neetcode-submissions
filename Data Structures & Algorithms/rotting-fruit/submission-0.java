6ece13e31d512c2813f1803cfb486b415993319c
th1993kim <81105748+th1993kim@users.noreply.github.com>
2026-08-02 17:38:40 +0900

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    private static int[][] move = new int[][]{{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
+    public int orangesRotting(int[][] grid) {
+        Queue<int[]> rottenQueue = new ArrayDeque<>();
+        int total = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] != 0) total++;
+                if (grid[i][j] == 2) {
+                    total--;
+                    rottenQueue.offer(new int[]{i, j});
+                }
+            }
+        }
+
+        int answer = 0;
+        while (!rottenQueue.isEmpty()) {
+            int queueSize = rottenQueue.size();
+
+            for (int i = 0; i < queueSize; i++) {
+                int[] rotten = rottenQueue.poll();
+                int y = rotten[0];
+                int x = rotten[1];
+
+                for (int k = 0; k < move.length; k++) {
+                    int ny = y + move[k][0];
+                    int nx = x + move[k][1];
+
+                    if (nx < 0 || ny < 0 || ny > grid.length - 1 || nx > grid[0].length -1) continue;
+                    if (grid[ny][nx] == 1) {
+                        total--;
+                        grid[ny][nx] = 2;
+                        rottenQueue.offer(new int[]{ny, nx});
+                    }
+                }
+            }
+
+            if (!rottenQueue.isEmpty()) answer++;
+        }
+        return total != 0 ? -1 : answer;
+    }
+}
