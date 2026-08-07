c9abccf890c0dbb41439ee94d7d0a62c826fe398
th1993kim <81105748+th1993kim@users.noreply.github.com>
2026-08-07 19:39:23 +0900

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = Math.max(dp[i][j], 
+                (i-1 < 0 ? 0 : dp[i-1][j]) + (j-1 < 0 ? 0 : dp[i][j-1]));
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+}
